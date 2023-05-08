c7373b25b8ff291647eeebe3e3bf561bdd221145
Leonardo Gonzalez <leo.gonzalez.paez@gmail.com>
2023-05-08 00:50:54 -0300

    v.0.0.2

--- js/calc_HC.js
+++ js/calc_HC.js
@@ -11,24 +11,33 @@ const diasVerano = 135;
 
 // ? Factores de Emisión en función de la fuente de emisión
 // por ahora simplemente declarativo deberian venir de una tabla
-// *************************************************************
-// * Fuente de emisión  **	Factor Emisión (KgCO₂eq/UM) ** UM **
-// *************************************************************
-// * PC Escritorio      **	 0,35                       ** h  **
-// * PC Portátil	    **   0,010692                   ** h  **
-// * Multifunción	    **   0,4374                     ** h  **
-// * Fotocopiadora	    **   0,4374                     ** h  **
-// * Impresora	        **   0,3645                     ** h  **
-// * Freezer	        **   0,04374                    ** h  **
-// * Heladera	        **   0,030618                   ** h  **
-// * Microondas	        **   0,31104                    ** h  **
-// * Dispenser 	        **   0,1215                     ** h  **
-// * Pava eléctrica	    **   1,1664                     ** h  **
-// * Radio	            **   0,02916                    ** h  **
-// *************************************************************
-let nombreFuente;
-let factorEmision;
-let horasUso; // horas de uso por día [hs/día]
+// *****************************************************************************************
+// * Grupo                  **  Fuente de emisión   **	Factor Emisión (KgCO₂eq/UM) ** UM **
+// *****************************************************************************************
+// * Artefactos Eléctricos  **    PC Escritorio     **	 0,35                       ** h  **
+// * Artefactos Eléctricos  **    PC Portátil	    **   0,010692                   ** h  **
+// * Artefactos Eléctricos  **    Multifunción	    **   0,4374                     ** h  **
+// * Artefactos Eléctricos  **    Fotocopiadora	    **   0,4374                     ** h  **
+// * Artefactos Eléctricos  **    Impresora	        **   0,3645                     ** h  **
+// * Artefactos Eléctricos  **    Freezer	        **   0,04374                    ** h  **
+// * Artefactos Eléctricos  **    Heladera	        **   0,030618                   ** h  **
+// * Artefactos Eléctricos  **    Microondas	    **   0,31104                    ** h  **
+// * Artefactos Eléctricos  **    Dispenser 	    **   0,1215                     ** h  **
+// * Artefactos Eléctricos  **    Pava eléctrica	**   1,1664                     ** h  **
+// * Artefactos Eléctricos  **    Radio	            **   0,02916                    ** h  **
+// *****************************************************************************************
+const tablaArtefactosElectricos = []; // decalro un array vacio para luego cargar los objetos
+
+tablaArtefactosElectricos.push(new ArtefactosElectricos("PC Escritorio",0.35))
+tablaArtefactosElectricos.push(new ArtefactosElectricos("PC Portátil",0.010692))
+tablaArtefactosElectricos.push(new ArtefactosElectricos("Multifunción",0.4374))
+tablaArtefactosElectricos.push(new ArtefactosElectricos("Fotocopiadora",0.4374))
+tablaArtefactosElectricos.push(new ArtefactosElectricos("Impresora",0.3645))
+tablaArtefactosElectricos.push(new ArtefactosElectricos("Microondas",0.31104))
+tablaArtefactosElectricos.push(new ArtefactosElectricos("Dispenser",0.1215))
+tablaArtefactosElectricos.push(new ArtefactosElectricos("Pava eléctrica",1.1664))
+
+console.log(tablaArtefactosElectricos)
 
 // ? Totales
 let totalEmisionArtefacto = 0;
@@ -50,6 +59,7 @@ let continuarApartado1; // si ó no
 let continuarApartado2; // si ó no
 let continuarApartado3; // si ó no
 
+
 // ! Inicio
 
 alert('Bienvenido al calculadora de su Hella de Carbono por las actividades que realizás en el ámbito laboral anualmente');
@@ -57,14 +67,11 @@ nombreUsuario = prompt("Ingrese su nombre");
 alert('Buenos días ' + nombreUsuario + ' comencemos con la carga de datos');
 cargaApartado1 = prompt('Quiere cargar los datos correspondientes a los consumo de Artefactos Eléctricos (si/no)');
 if  (cargaApartado1 == 'si') {
-    do {
-        nombreFuente = prompt('Ingrese el nombre del Artefactos Eléctricos');
-        factorEmision = Number(prompt('Ingrese el valor de Factor Emisión [KgCO₂eq/UM]'));
-        horasUso = Number(prompt('¿Cantidad de horas que se usa el artefacto por día?'))
-        totalEmisionArtefacto = totalEmisionArtefacto + factorEmision * horasUso * diasLaborales
-        continuarApartado1 = prompt("¿Desea ingresar otro Artefactos Eléctrico? (si/no)");
-    } while (continuarApartado1 == 'si');
-
+    for (ArtefactosElectricos of tablaArtefactosElectricos){
+        ArtefactosElectricos.horas_uso = Number(prompt(`¿Cantidad de horas que se usa el ${ArtefactosElectricos.artefacto} por día?`));
+        ArtefactosElectricos.emisiones= ArtefactosElectricos.horas_uso * ArtefactosElectricos.factor_emision * diasLaborales;
+        totalEmisionArtefacto = totalEmisionArtefacto + ArtefactosElectricos.emisiones
+    }
     alert('El total de emisiones debido al uso de artefactos eléctricos es de ' + totalEmisionArtefacto + ' KgCO₂eq');
     cargaApartado2 = prompt('Quiere cargar los datos correspondientes a los consumo para la Climatización (si/no)');
 
@@ -77,7 +84,3 @@ if  (cargaApartado1 == 'si') {
 } else if (cargaApartado3 == 'si') {
 
 }
-
-
-
-
